22fa263b79a4ff2b94ff156cff017d8e524d4a5c
Eric D <ericdanziger@cmu.edu>
2015-02-23 18:10:31 -0500

    changing spin

--- src/locom_main.cpp
+++ src/locom_main.cpp
@@ -64,15 +64,15 @@ int main(int argc, char **argv)
   locomotion_module->pub_right = nh.advertise<std_msgs::Float32>("velRight", 10);
   
   ros::Subscriber sub_message = nh.subscribe("cmd_vel", 1000, &LocomotionModule::messageCallback, locomotion_module);
- 
+  ros::spin();
   // Main loop.
-  while (nh.ok())
-  {
-    // Publish the message. Do this in the callback for subscribing? 
+  // while (nh.ok())
+  // {
+  //   // Publish the message. Do this in the callback for subscribing? 
     
-    ros::spinOnce();
-    //r.sleep();
-  }
+  //   ros::spinOnce();
+  //   //r.sleep();
+  // }
 
   return 0;
 } // end main()
