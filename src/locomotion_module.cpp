8eb7a1211a9532aeb573da95c5fab88b4da90ff4
Eric D <ericdanziger@cmu.edu>
2015-02-23 17:52:21 -0500

    fixing type

--- src/locomotion_module.cpp
+++ src/locomotion_module.cpp
@@ -28,6 +28,6 @@ void LocomotionModule::messageCallback(const geometry_msgs::Twist::ConstPtr &msg
 void LocomotionModule::configCallback(locomotion_module::locomotionModuleConfig &config, uint32_t level)
 {
   r_ = config.r + r_center_;
-  ROS_INFO("R changed %s",r_);
+  ROS_INFO("R changed %f",r_);
 
 }
